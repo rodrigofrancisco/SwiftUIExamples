8bf0073aa1f58845e439bfb41fb8e05867a22d27
Rodrigo Francisco <rodrigo.faw@gmail.com>
2024-09-07 23:06:09 -0600

    Add basic project to show case swifui functionalities

---
+++ ProductListingPage/ProductListingPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProductListingPage
+//
+//  Created by Rodrigo Francisco on 07/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProductListingPage/ProductListingPage/ProductListingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListingPageApp.swift
+//  ProductListingPage
+//
+//  Created by Rodrigo Francisco on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ProductListingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
